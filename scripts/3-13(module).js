4ee225aef431770995acf9c4d22ebe80b069641a
김서연 <ksyssyy070814@gmail.com>
2024-11-21 22:44:55 +0900

    JS 수행 준비

--- scripts/3-13(module).js
+++ scripts/3-13(module).js
@@ -17,3 +17,5 @@ export function error(message) {
 }
 
 export const PI = 3.14;
+
+// export = 다른 모듈에서 사용할 수 있도록 변수, 함수, 클래스 등을 내보내는 것이야.
--- scripts/3-13(ref).js
+++ scripts/3-13(ref).js
@@ -1,3 +1,4 @@
 import {log} from "/scripts/3-13(module).js"
 
+// /scripts/3-13(module).js에서 정의된 log 함수가 호출되어 메시지를 콘솔에 출력.
 log("이소리 바보 멍청이")
